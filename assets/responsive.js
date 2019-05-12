790456b8f43c1bb0ff7588875963c111d13da77c
Alex-Cannon <alexcannon2018@gmail.com>
2019-05-12 14:57:06 -0600

    feat: navbar mobile-responsive

--- assets/responsive.js
+++ assets/responsive.js
@@ -1,31 +1,42 @@
-const hamburger = `
-  <div style="display:flex; flex-direction: column;">
-    <a href="/">Blog</a>
-    <a href="/about">About</a>
-    <a href="/portfolio">Portfolio</a> 
+const tablet = 768;
+let open = false;
+const burger = `
+  <div id="burger" class="burger-close">
+    <span class="bar1"></span>
+    <span class="bar2"></span>
+    <span class="bar3"></span>
   </div>
 `;
-
-const defaultNavRight = `
+const links = `
   <a href="/">Blog</a>
   <a href="/about">About</a>
   <a href="/portfolio">Portfolio</a> 
 `;
 
-(function () {
-  const desktopBreakpoint = 768;
-
-  function updateHamburger() {
-    if (window.innerWidth < desktopBreakpoint) {
-      get('nav-right').innerHTML = defaultNavRight;
-    } else {
-      get('nav-right').innerHTML = defaultNavRight;
-    }
+function toggleMenu() {
+  open = !open;
+  if (open) {
+    get('burger').className = 'burger-open';
+    get('mobile-menu').innerHTML = links;
+  } else {
+    get('burger').className = 'burger-close';
+    get('mobile-menu').innerHTML = '';
   }
+}
 
-  function get(id) {
-    return document.getElementById(id);
+function updateHamburger() {
+  if (window.innerWidth < tablet) {
+    get('nav-right').innerHTML = burger;
+    get('burger').addEventListener('click', toggleMenu);
+  } else {
+    get('nav-right').innerHTML = links;
+    open = false;
   }
+}
 
-  window.addEventListener('resize', updateHamburger);
-})()
+function get(id) {
+  return document.getElementById(id);
+}
+
+window.addEventListener('resize', updateHamburger);
+updateHamburger();
